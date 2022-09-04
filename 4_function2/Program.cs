9617cba7f8ba79647807fa4d529e4a3b461c024c
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-09-04 15:06:23 +0300

    Update Program.cs

--- 4_function2/Program.cs
+++ 4_function2/Program.cs
@@ -2,34 +2,60 @@
 
 namespace _4_function2
 {
-    internal class Program
+internal class Program
     {
         static void Main(string[] args)
         {
-            double health = 0, mana = 0;
-            int maxHealth = 10, maxMana = 10;
-            double maximumLength = 10, maximumPercentage = 100;
-            
-            while (true)
-            {
-                Console.WriteLine("Healthbar");
-                DrawBar((int)health, maxHealth, ConsoleColor.Red, 1, '_');
-                Console.WriteLine();
-                Console.WriteLine("\nManabar");
-                DrawBar((int)mana, maxMana, ConsoleColor.Blue, 4, '_');
+            double health = 0;
+            double mana = 0;
+            int maxHealth = 10;
+            int maxMana = 10;
+            double maximumLength = 10;
+            double maximumPercentage = 100;
+            string userInput;
+            bool canExitApp = true;
 
+            while (canExitApp)
+            {
+                ShowBar(health, mana, maxHealth, maxMana);
+               
                 Console.SetCursorPosition(0, 6);
 
                 Console.Write("Введите число на которое изменится жизнь в процентах: ");
                 health = int.Parse(Console.ReadLine()) / maximumPercentage * maximumLength;
                 Console.Write("Введите число на которое изменится мана в процентах: ");
                 mana = int.Parse(Console.ReadLine()) / maximumPercentage * maximumLength;
-
-                Console.ReadKey();
+                
                 Console.Clear();
-            }
+
+                ShowBar(health, mana, maxHealth, maxMana);
+
+                Console.WriteLine("\n\nДля выхода введите - exit");
+                Console.WriteLine("\nЧтобы заполнить бары ещё раз нажмите любую клавишу...");
+                Console.Write("\nВвод: ");
+                
+                userInput = Console.ReadLine();
+
+                if(userInput == "exit")
+                {
+                    canExitApp = false;
+                }
+                else
+                {
+                    Console.Clear();
+                }
+            }            
         }
 
+        static void ShowBar(double health, double mana, int maxHealth, int maxMana)
+        {
+            Console.WriteLine("Healthbar");
+            DrawBar((int)health, maxHealth, ConsoleColor.Red, 1, '_');
+            Console.WriteLine();
+            Console.WriteLine("\nManabar");
+            DrawBar((int)mana, maxMana, ConsoleColor.Blue, 4, '_');
+        }
+        
         static void DrawBar(int value,  int MaxValue, ConsoleColor color, int position, char symbol = ' ', string sign = "#")
         {
             ConsoleColor defaultColor = Console.BackgroundColor;
